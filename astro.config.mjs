86c13f3f49d5b5f7a385ae06bfff9a82ec9ce19f
Nicolas-DL <nicolas.dupont@outlook.com>
2024-12-30 20:25:52 +0100

    adaptation pour github

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,8 @@ import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://NicoDupont.github.io',
+  base: 'cvnicolasdl',
+  trailingSlash: "always",
   integrations: [tailwind(),icon()]
 });
